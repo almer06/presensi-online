be604dc9d34840ab549d3a320774af5fa1e51e04
almer06 <almer.maulana1234@gmail.com>
2024-11-19 21:45:45 +0700

    added default user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,6 +22,16 @@ class DatabaseSeeder extends Seeder
 //        ]);
         $this->call([
             AdminSeeder::class,
+        ]);
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password1234'),
+            'username' => 'admin',
+            'tempat_lahir' => 'Jakarta',
+            'tanggal_lahir' => '1990-01-01',
+            'address' => 'Jakarta',
+            'nik' => '1234567890123456',
         ]);
     }
 }
